768640c8322df99184fa7d1f54375883deb8566a
TollyH <ptolemyhill@sky.com>
2024-06-04 21:48:21 +0100

    Implement executable loading

---
+++ DialogPopup.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace AssEmbly.DebuggerGUI
+{
+    /// <summary>
+    /// Interaction logic for DialogPopup.xaml
+    /// </summary>
+    public partial class DialogPopup : Window
+    {
+        public const string ErrorIcon = "pack://application:,,,/Icons/cross-circle-32.png";
+
+        public DialogPopup(string message, string title, string image)
+        {
+            InitializeComponent();
+
+            Title = title;
+            messageText.Text = message;
+            dialogImage.Source = new BitmapImage(new Uri(image));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
+using Microsoft.Win32;
 
 namespace AssEmbly.DebuggerGUI
 {
@@ -7,9 +9,57 @@ namespace AssEmbly.DebuggerGUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Processor? DebuggingProcessor { get; private set; }
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void LoadExecutable(string path)
+        {
+            AAPFile executable;
+            try
+            {
+                executable = new(File.ReadAllBytes(path));
+            }
+            catch (Exception exc)
+            {
+                DialogPopup popup = new(exc.Message, "Error loading executable", DialogPopup.ErrorIcon)
+                {
+                    Owner = this
+                };
+                popup.ShowDialog();
+                return;
+            }
+
+            if (!ulong.TryParse(memorySizeInput.Text, out ulong memorySize))
+            {
+                DialogPopup popup = new("Entered memory size is not valid.", "Error creating processor", DialogPopup.ErrorIcon)
+                {
+                    Owner = this
+                };
+                popup.ShowDialog();
+                return;
+            }
+
+            try
+            {
+                DebuggingProcessor = new Processor(memorySize, executable.EntryPoint,
+                    forceV1.IsChecked ?? false, mapStack.IsChecked ?? true, autoEcho.IsChecked ?? false);
+                DebuggingProcessor.LoadProgram(executable.Program);
+            }
+            catch (Exception exc)
+            {
+                DialogPopup popup = new(exc.Message, "Error creating processor", DialogPopup.ErrorIcon)
+                {
+                    Owner = this
+                };
+                popup.ShowDialog();
+                return;
+            }
+
+            executablePathText.Text = path;
         }
 
         private void AboutItem_Click(object sender, RoutedEventArgs e)
@@ -19,6 +69,21 @@ namespace AssEmbly.DebuggerGUI
                 Owner = this
             };
             window.ShowDialog();
+        }
+
+        private void OpenItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new()
+            {
+                CheckFileExists = true,
+                CheckPathExists = true,
+                Filter = "Assembled AssEmbly Programs (*.aap)|*.aap",
+                Title = "Open Executable"
+            };
+            if (dialog.ShowDialog(this) ?? false)
+            {
+                LoadExecutable(dialog.FileName);
+            }
         }
     }
 }
